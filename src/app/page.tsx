01759e08cab6579bce89e4777b602b31fd3b91d7
ale0110 <carosettialessandro@gmail.com>
2025-07-21 12:53:38 +0200

    nuovo deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function MenuPage() {
             height={16}
             className="inline-block"
           />
-          Menù Italiano
+          Menù Italiano 
         </a>
 
         <a
